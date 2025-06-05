4002e41e2e06bde6c3a5e43caa789e7441b839be
Indhumathi-SivaShanmugam <indhusmis@gmail.com>
2025-06-05 11:10:40 +0530

    practicing

---
+++ src/Armstrong.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] number= String[] number=scanner.nextInt();
+
+
+
+    }
+}
