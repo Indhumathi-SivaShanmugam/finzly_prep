5e9a977b5476f802610b66591d536d305e001b8f
Indhumathi-SivaShanmugam <indhusmis@gmail.com>
2025-06-05 15:13:29 +0530

    practicing functions of different return and access specifiers

---
+++ src/Functions_trial.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Functions_trial {
+    public static boolean isEven(int number){
+        if(number%2==0){
+            System.out.println(number +" is Even");
+            return true;
+        }
+        else{
+            System.out.println(number+" is odd");
+            return false;
+        }
+    }
+
+    public static int MaxOf2(int a , int b){
+        if(a>b){
+            System.out.printf("Max is "+ a);
+            return a;
+        }
+        else{
+            System.out.printf("Max is "+ b);
+            return b;
+        }
+    }
+
+    private static double convertlogic(double celsius){
+        return ((celsius * 9/5) +32);
+    }
+
+    public static void DisplayF(double celsius){
+        double display=convertlogic(celsius);
+        System.out.println("The Temp in celcius is "+ display);
+    }
+    public static void main(String[] args) {
+       isEven(5);
+       isEven(8);
+       MaxOf2(5,8);
+       Scanner scanner=new Scanner(System.in);
+       System.out.println("Enter Temp in Celcius");
+       double celcius= scanner.nextDouble();
+       DisplayF(celcius);
+
+
+    }
+}
